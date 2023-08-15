84b5ff0a0994238733b50d09e87934c29b5bbadd
ClayUA <claytruelove@gmail.com>
2023-08-14 21:24:43 -0500

    added some button design put the 4 sub sections into an even grid

--- client/src/Body.js
+++ client/src/Body.js
@@ -13,8 +13,7 @@ export default function Body() {
                     Hositng 255 members, we pride ourselves on our ability to foster a passion for the world of computer science as well as give you the tools in order to thrive in the Industry post college!
                 </p>
             </div>
-            <img className='about-pic' src={geoffry} alt='about pic' />
-            <img className='prof-pic' src={geoffry} alt='about pic' />
+           
             <div className='professionalism-section'>
                 <h3 className='prof-header'>Professionalism</h3>
                 <p className='prof-p'>
@@ -22,22 +21,21 @@ export default function Body() {
                 </p>
             </div>
             <div className='coding-section'>
+
                 <h3 className='coding-header'>Coding</h3>
                 <p className='coding-p'>
                     Coding section
                 </p>
             </div>
-            <img className='coding-pic' src={geoffry} alt='about pic' />
-            <img className='social-pic' src={geoffry} alt='about pic' />
+          
             <div className='social-section'>
                 <h3 className='social-header'>Social</h3>
                 <p className='social-p'>
                     Social Section
                 </p>
             </div>
-            <div className='join-btn-container'>
-                <button className='join-btn'>Register Now!</button>
-            </div>
+            
+                <button className='join-btn'>Register Now</button>
         </div>
     </>
   )
