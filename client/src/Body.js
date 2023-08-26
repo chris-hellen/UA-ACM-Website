92f00ac0a60910af351eccd9f0aab4aead1f8b3d
ClayUA <claytruelove@gmail.com>
2023-08-25 19:55:19 -0500

    added images to the container

--- client/src/Body.js
+++ client/src/Body.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import "./Body.css"
-import geoffry from './Geoffry.PNG'
+import geoffry from "./Geoffry.PNG"
+
+
 
 export default function Body() {
   return (
     <>
+       
+       
         <div className='grid-container'>
             <div className='about-section'>
+            <img className='about-pic' src={geoffry} />
                 <h3 className='about-header'>About Us</h3>
                 <p className='about-p'>
-                    UA ACM is the leading Computer Science club here at The Capstone.
-                    Hositng 255 members, we pride ourselves on our ability to foster a passion for the world of computer science as well as give you the tools in order to thrive in the Industry post college!
+                    description
                 </p>
             </div>
            
             <div className='professionalism-section'>
+            <img className='prof-pic' src={geoffry} />
                 <h3 className='prof-header'>Professionalism</h3>
                 <p className='prof-p'>
                     Professionalism section
                 </p>
             </div>
             <div className='coding-section'>
-
+            <img className='coding-pic' src={geoffry} />
                 <h3 className='coding-header'>Coding</h3>
                 <p className='coding-p'>
                     Coding section
@@ -29,6 +34,7 @@ export default function Body() {
             </div>
           
             <div className='social-section'>
+            <img className='social-pic' src={geoffry} />
                 <h3 className='social-header'>Social</h3>
                 <p className='social-p'>
                     Social Section
