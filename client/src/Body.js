eab3bea7199b40ecf7539c79817bd3af6634eb36
derringercj <124228065+derringercj@users.noreply.github.com>
2023-08-28 19:52:02 -0500

    Wrote officer section, changed colors, imgs

--- client/src/Body.js
+++ client/src/Body.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import "./Body.css"
-import aboutus from "./about-us.png"
-import profpic from "./profpic.png"
-import codingpic from "./codingpic.png"
-import socialpic from "./socialpic.png"
+import geoffry from "./Geoffry.PNG"
+import treasurer from "./Treasurer.jpg"
 
 
 
@@ -14,7 +12,7 @@ export default function Body() {
        
         <div className='grid-container'>
             <div className='about-section'>
-            <img className='about-pic' src={aboutus} />
+            <img className='about-pic' src={geoffry} />
                 <h3 className='about-header'>About Us</h3>
                 <p className='about-p'>
                 UA ACM is the leading Computer Science club here at The Capstone. We pride ourselves on our ability to foster a passion for the world of computer science as well as give you the tools to thrive in the industry post-graduation!
@@ -22,7 +20,7 @@ export default function Body() {
             </div>
            
             <div className='professionalism-section'>
-            <img className='prof-pic' src={profpic} />
+            <img className='prof-pic' src={geoffry} />
                 <h3 className='prof-header'>Professionalism</h3>
                 <p className='prof-p'>
                     Bringing in companies from across the country, we put you in touch with 
@@ -31,7 +29,7 @@ export default function Body() {
                 </p>
             </div>
             <div className='coding-section'>
-            <img className='coding-pic' src={codingpic} />
+            <img className='coding-pic' src={geoffry} />
                 <h3 className='coding-header'>Coding</h3>
                 <p className='coding-p'>
                     Whether it be coding workshops, or hackathon opportunities, 
@@ -41,7 +39,7 @@ export default function Body() {
             </div>
           
             <div className='social-section'>
-            <img className='social-pic' src={socialpic} />
+            <img className='social-pic' src={geoffry} />
                 <h3 className='social-header'>Social</h3>
                 <p className='social-p'>
                     ACM puts you in touch with other Computer Science and Cybersecurity majors on
@@ -55,8 +53,26 @@ export default function Body() {
         <div class="officer-grid">
             <h1 class='officer-header'>Officers</h1>
             <h1 class='officer'>Daniel Tsark - President</h1>
+            <img class='treasurer' src={treasurer}/>
             <p>
-                Daniel Tsark is a cybersecurity major from Phoenix, Arizona. His body doesn't quit.
+                Daniel Tsark is a junior cybersecurity major from Phoenix, Arizona. He is also involved on campus with the Mu
+                Chapter of Theta Tau, and his body doesn't quit!
+            </p>
+            <h1 class='officer'>Angel Roa-Perez - Vice President</h1>
+            <img class='treasurer' src={treasurer}/>
+            <p>
+                I literally know nothing about this guy soooooo? He's VP
+            </p>
+            <h1 class='officer'>Liam Tucker - Secretary</h1>
+            <img class='treasurer' src={treasurer}/>
+            <p>
+                Liam Tucker is a computer science major from somewhere I assume? Don't know much about this guy either. 
+            </p>
+            <h1 class='officer'>Casey Derringer - Treasurer</h1>
+            <img class='treasurer' src={treasurer}/>
+            <p>
+                Casey Derringer is a sophomore computer science major from Dayton, Ohio. Along with serving as ACM Treasurer, he fences 
+                in the University of Alabama's fencing club.
             </p>
         </div>
         <footer>
